41e21e41db2ec73426ab8cd1d243302682f16063
Guilherme Rodrigo <101368184+guilherms@users.noreply.github.com>
2022-05-05 22:32:49 +0000

    wtf

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Users(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100))
   email = db.Column(db.String(100))
